c5c8bca7483f3c9c5755b67e256781a3c83412d2
Tamulus47 <mahmoud.mostafa0047@gmail.com>
2022-09-21 16:02:56 +0200

    test data collector function

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,24 @@
-import express  from "express"
-const app = express()
-const port = 8080
+import express from 'express';
+import fs from 'fs';
+import path from 'path';
+const app = express();
+const port = 8080;
 
 app.listen(port, () => {
-  console.log(`server is running on port ${port}`)
-})
+  console.log(`server is running on port ${port}`);
+});
 
 app.get('/', (req, res) => {
   res.status(200).send('server is up');
-})
+});
+
+function readimages(dir: string) {
+  const files: string[] = [];
+  fs.readdirSync(dir).forEach((filename) => {
+    const name = path.parse(filename).name + path.parse(filename).ext;
+    files.push(name);
+  });
+  return files;
+}
+readimages('images');
+console.log(readimages('images'));
