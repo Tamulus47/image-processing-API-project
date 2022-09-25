ae3eadac2f89e4dcc0ed74a8976059220a512f92
Tamulus47 <mahmoud.mostafa0047@gmail.com>
2022-09-25 16:09:14 +0200

    prettier script and configure with lint script

--- src/tests/helpers/reporter.ts
+++ src/tests/helpers/reporter.ts
@@ -1,19 +1,23 @@
 /* eslint-disable prettier/prettier */
-import { DisplayProcessor, SpecReporter, StacktraceOption } from 'jasmine-spec-reporter'
-import SuiteInfo = jasmine.SuiteInfo
+import {
+  DisplayProcessor,
+  SpecReporter,
+  StacktraceOption
+} from 'jasmine-spec-reporter';
+import SuiteInfo = jasmine.SuiteInfo;
 
 class CustomProcessor extends DisplayProcessor {
   public displayJasmineStarted(info: SuiteInfo, log: string): string {
-    return `TypeScript ${log}`
+    return `TypeScript ${log}`;
   }
 }
 
-jasmine.getEnv().clearReporters()
+jasmine.getEnv().clearReporters();
 jasmine.getEnv().addReporter(
   new SpecReporter({
     spec: {
-      displayStacktrace: StacktraceOption.NONE,
+      displayStacktrace: StacktraceOption.NONE
     },
-    customProcessors: [CustomProcessor],
+    customProcessors: [CustomProcessor]
   })
-)
+);
